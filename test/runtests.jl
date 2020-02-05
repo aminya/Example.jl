046c447dd12ba7de02f8e21a63ce55cb395ae802
aminya <aminyahyaabadi74@gmail.com>
2020-02-04 21:27:42 -0600

    static test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,10 @@
 using Test, Example
 
-@test hello("Julia") == "Hello, Julia"
-@test domath(2.0) ≈ 7.0
+
+@static if Sys.iswindows()
+  @test hello("Julia") == "Hello, Julia"
+
+  elseif Sys.islinux()
+    @test domath(2.0) ≈ 7.0
+
+  end
